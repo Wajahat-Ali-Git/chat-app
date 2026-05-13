8f22efbaf9d4d18c193aa54e30d81862d2c1395c
Wajahat-Ali-Git <wajahatali20012@gmail.com>
2026-05-13 16:46:53 +0500

    feat: updated the frontend ui of the sign in and sign up page

--- chat-app-frontend/app/login/page.tsx
+++ chat-app-frontend/app/login/page.tsx
@@ -1,22 +1,39 @@
-"use-client;";
+"use client";
+
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <section className="w-full h-screen flex items-center justify-center">
-        <section className="flex flex-col items-center justify-center gap-6">
+        <section className="flex flex-col md:w-[400px] items-center justify-center gap-6">
           <div
             className="flex flex-col
           "
           >
             <h1 className="text-[50px]">Login</h1>
-            <p className="text-[20px]">Sign in to your account</p>
-            <label>Email</label>
-            <input id="email" type="email" />
-            <label>Password</label>
-            <input id="password" type="password" />
+            <p className="text-[20px] opacity-75">Sign in to your account</p>
+            <label className="font-bold">Email: </label>
+            <input
+              id="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label className="font-bold">Password:</label>
+            <input
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
               Login
             </button>
+            {email}
+            <br />
+            {password}
           </div>
         </section>
       </section>
--- chat-app-frontend/app/signup/page.tsx
+++ chat-app-frontend/app/signup/page.tsx
@@ -1,5 +1,10 @@
-"use-client;";
+"use client";
+import { useState } from "react";
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <section className="w-full h-screen flex items-center justify-center">
@@ -9,16 +14,37 @@ const SignUp = () => {
           "
           >
             <h1 className="text-[50px]">Sign Up</h1>
-            <p className="text-[20px]">Create an account to get started</p>
-            <label >Name</label>
-            <input id="name" type="text" />
-            <label >Email</label>
-            <input id="email" type="email" />
-            <label >Password</label>
-            <input id="password" type="password" />
+            <p className="text-[20px] opacity-75">Create an account to get started</p>
+            <label className="font-bold">Name:</label>
+            <input
+              id="name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label className="font-bold">Email:</label>
+            <input
+              id="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label className="font-bold">Password:</label>
+            <input
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+             <label className="font-bold">confirm Password:</label>
+            <input
+              id="confirmPassword"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
               Sign Up
             </button>
+            {email}
+            <br />
+            {password}
           </div>
         </section>
       </section>
